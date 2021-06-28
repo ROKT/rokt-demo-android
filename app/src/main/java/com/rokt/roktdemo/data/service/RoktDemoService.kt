06c20e10f72e36d8174c5a9c6e502febb515691c
Neda Meily <neda.meily@rokt.com>
2021-06-28 12:36:53 +1000

    remove headers

--- app/src/main/java/com/rokt/roktdemo/data/service/RoktDemoService.kt
+++ app/src/main/java/com/rokt/roktdemo/data/service/RoktDemoService.kt
@@ -3,15 +3,12 @@ package com.rokt.roktdemo.data.service
 import com.rokt.roktdemo.model.AboutRokt
 import com.rokt.roktdemo.model.DemoLibrary
 import retrofit2.http.GET
-import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface RoktDemoService {
-    @Headers("accept: text/plain")
     @POST("library")
     suspend fun getDemoLibrary(): DemoLibrary
 
-    @Headers("accept: text/plain")
     @GET("about")
     suspend fun getAboutPage(): AboutRokt
 }
