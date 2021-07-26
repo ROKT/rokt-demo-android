77f1db75a3d97ba740fc0779385413e069488bd3
danial motahari <danial.motahari@rokt.com>
2021-07-26 18:18:21 +1000

    update home page logo

--- app/src/main/java/com/rokt/roktdemo/ui/demo/DemoHome.kt
+++ app/src/main/java/com/rokt/roktdemo/ui/demo/DemoHome.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -56,6 +57,7 @@ private fun DemoPageSuccess(
 ) {
     Column(
         Modifier
+            .fillMaxSize()
             .background(MaterialTheme.colors.surface)
     ) {
         Box(Modifier.padding(PaddingValues(start = 3.dp, top = 54.dp))) {
