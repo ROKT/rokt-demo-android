12c31bf08b3b3f30bf5d6422cece273e4bab619a
Thomson Thomas <thomson.thomas@rokt.com>
2023-07-05 20:10:38 +1000

    Version update - use DCUI 4.0.0 and make compatible
    Network debug settings
    Essential version updates

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.gradle.kotlin.dsl.`kotlin-dsl`
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `kotlin-dsl`
@@ -6,4 +7,15 @@ plugins {
 
 repositories {
     mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 }
