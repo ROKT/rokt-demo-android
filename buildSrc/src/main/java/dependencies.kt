b3bafcf4337e1a6d3e49bb3d07ae571be4116ebf
Sahil Suri <sahil.suri@rokt.com>
2024-06-21 11:03:33 +1000

    No_Jira Updated Coil Version to 2.2.0

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -10,7 +10,7 @@ object Libs {
     const val rokt = "com.rokt:roktsdk:4.3.1"
     const val timber = "com.jakewharton.timber:timber:4.7.1"
     const val okhttp = "com.squareup.okhttp3:okhttp:4.9.0"
-    const val coil = "io.coil-kt:coil-compose:2.6.0"
+    const val coil = "io.coil-kt:coil-compose:2.2.0"
     const val zxing = "com.journeyapps:zxing-android-embedded:4.3.0"
 
     object Retrofit {
